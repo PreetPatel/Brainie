ff810e54e058132eeb545cd111b732a78773160a
Preet Patel <iam@preetpatel.com>
2021-06-05 13:45:52 +1200

    Initial Commit

---
+++ Brainie/BrainieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainieApp.swift
+//  Brainie
+//
+//  Created by Preet Patel on 5/06/21.
+//
+
+import SwiftUI
+
+@main
+struct BrainieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
